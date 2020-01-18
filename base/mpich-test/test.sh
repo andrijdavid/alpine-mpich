aabc747a31710ac1761929d810b4fbdf8847d402
andrijdavid <david@geek.mg>
2020-01-18 13:36:57 +0100

    Update test.sh

--- base/mpich-test/test.sh
+++ base/mpich-test/test.sh
@@ -4,17 +4,17 @@ set -e
 echo "--- Test MPICH installation ---"
 
 printf "it should find mpicc... "
-mpicc --version > /dev/null
+mpicc --version
 echo ok
 
 printf "it should find mpiexec... "
-mpiexec --version > /dev/null
+mpiexec --version
 echo ok
 
 printf "it should compile mpi_hello_world.c source... "
-mpicc -o mpi_hello_world mpi_hello_world.c > /dev/null
+mpicc -o mpi_hello_world mpi_hello_world.c
 echo ok
 
 printf "it should run mpi_hello_world program successfully... "
-mpirun -n 4 ./mpi_hello_world > /dev/null
+mpirun -n 4 ./mpi_hello_world
 echo ok
